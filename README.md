c254cbb6494520ee01a66f71ef4365feb5c58015
Jesse Stricker <git@jessestricker.de>
2024-03-02 10:11:31 +0100

    build: create project

---
+++ README.md
@@ -0,0 +1,8 @@
+<!--
+ Copyright 2024 Jesse Stricker.
+ SPDX-License-Identifier: Apache-2.0
+-->
+
+# upper
+
+Upgrade all your packages!
---
+++ upper/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 Jesse Stricker.
+# SPDX-License-Identifier: Apache-2.0
+
+
+def main() -> int | None:
+    print("hello")
